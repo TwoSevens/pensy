643e9d4cb377c609ab111a7cfaa1892b81be25a7
Mohammed Adel <mohammed28adel@gmail.com>
2026-05-16 23:38:53 +0300

    Initialize project.

---
+++ shell.nix
@@ -0,0 +1,20 @@
+# Run with `nix-shell shell.nix`
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    wrapGAppsHook4
+    cargo 
+    cargo-tauri
+    nodejs 
+    nodePackages.pnpm
+    rustc # Needed for dev server (pnpm tauri dev)
+  ];
+
+  buildInputs = with pkgs; [
+    librsvg
+    webkitgtk_4_1
+  ];
+}
